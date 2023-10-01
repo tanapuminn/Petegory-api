70e6b1b5ab2d7739457014e2449af2b6dec61fca
tanapuminn <tanapumin.fo@gmail.com>
2023-10-01 19:59:31 +0700

    change api sheet

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const app = express();
 app.use(
   cors({
     origin: ['http://localhost:3000'],
-    methods: ['POST', 'GET', 'PUT', 'DELETE'],
+    methods: ['POST', 'GET', 'PUT', 'DELETE', 'OPTIONS'],
     credentials: true,
   })
 );
